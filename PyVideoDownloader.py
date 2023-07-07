9f355c0d3bb91d8e9d2f20a7bd60b6253f5745d8
Bryan de Oliveira <bryan.oliveira12345@gmail.com>
2023-07-07 17:22:40 -0300

    changed Scripts to scripts

--- PyVideoDownloader.py
+++ PyVideoDownloader.py
@@ -2,7 +2,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from Scripts import MainWindowClass
+from scripts import MainWindowClass
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
